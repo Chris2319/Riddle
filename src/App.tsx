7a0e9cb7e31a7b753ef978b481f832c46b688402
Chris <chris@chimneydev.co.za>
2024-08-25 14:46:22 +0200

    animation updates

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,16 @@ import {AnimatePresence, motion} from 'framer-motion';
 import jupiterImage from './assets/jupiter+nasa+photo.jpg';
 import orionImage from './assets/Orion.jpg';
 import europaImage from './assets/Europa.webp';
-import lagrangeImage from './assets/Lagrange.webp';
+import lagrangeImage from './assets/Lagrange.jpg';
 import saturnImage from './assets/saturn.jpg';
-import betelgeuseImage from './assets/Betelgeuse.webp';
+import betelgeuseImage from './assets/beetle.webp';
 import ioImage from './assets/Io.jpg';
 import titanImage from './assets/Titan.jpeg';
 import callistoImage from './assets/Callisto.jpg';
-import hubbleImage from './assets/Hubble.webp';
+import hubbleImage from './assets/hubble.jpeg';
 import enceladusImage from './assets/Enceladus.jpg';
 import sunImage from './assets/sun.jpg';
+import Spline from '@splinetool/react-spline';
 
 
 interface QuizQuestion {
@@ -94,6 +95,14 @@ function App() {
         visible: {opacity: 1, transition: {duration: 2}},
         hidden: {opacity: 0, transition: {duration: 2}},
     };
+    const vLi = {
+        visible: {opacity: 1, x: 0},
+        hidden: {opacity: 1, x: '-100%'},
+    };
+    const vUl = {
+        visible: {transition: {duration: 2, staggerChildren: 0.2}},
+        hidden: {transition: {duration: 2, staggerChildren: 0.2}},
+    };
 
     useEffect(() => {
         const userAnswers = Object.values(answers as any) as any;
@@ -120,40 +129,73 @@ function App() {
         return answers?.[item.answer as keyof typeof answers]?.toLowerCase() === correctAnswers[index]?.toLowerCase();
     };
 
-    const removeBlur = () => {
+    const getStylePerImage = (item: QuizQuestion) => {
+        let style = {
+            backgroundImage: `url(${answerImages[item.answer]})`,
+            backgroundPosition: 'center bottom',
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+            transition: 'all 800ms ease-in-out'
+        };
+        switch (item.answer){
+            case 'Jupiter':
+                style.backgroundPosition = '10rem 5rem';
+                break;
+            case 'Europa':
+                style.backgroundPosition = '0 10rem';
+                break;
+            case 'Saturn':
+                style.backgroundPosition = 'center bottom';
+                style.backgroundSize = '110%';
+                break;
+            case 'Betelgeuse':
+                style.backgroundPosition = 'center 5rem';
+                break;
+            case 'Io':
+                style.backgroundPosition = '1rem 10rem';
+                style.backgroundSize = '150%';
+                break;
+            case 'Titan':
+                style.backgroundPosition = 'center 10rem';
+                break;
+            case 'Callisto':
+                style.backgroundPosition = 'center 8rem';
+                style.backgroundSize = '150%';
+                break;
+        }
+
+        return style;
+    }
+
+    const removeBlur = (item: QuizQuestion) => {
         const index = correctAnswers.findIndex((a) => a === activeItem.answer);
-        if (index > -1 && answers[activeItem?.answer as keyof typeof answers]?.toLowerCase() === correctAnswers[index]?.toLowerCase()) return true;
+        if (index > -1 && item.answer === activeItem.answer && answers[activeItem?.answer as keyof typeof answers]?.toLowerCase() === correctAnswers[index]?.toLowerCase()) return true;
         return false;
     };
 
     return (
         <div className={'wrapper'}>
             <AnimatePresence>
-                {/*<button onClick={() => setAnswers({*/}
-                {/*    'Jupiter': 'Jupiter',*/}
-                {/*    'Orion': 'Orion',*/}
-                {/*    'Europa': 'Europa',*/}
-                {/*    'Lagrange': 'Lagrange',*/}
-                {/*    'Saturn': 'Saturn',*/}
-                {/*    'Betelgeuse': 'Betelgeuse',*/}
-                {/*    'Io': 'Io',*/}
-                {/*    'Titan': 'Titan',*/}
-                {/*    'Callisto': 'Callisto',*/}
-                {/*    'Hubble': 'Hubble',*/}
-                {/*    'Enceladus': 'Enceladus',*/}
-                {/*    'Sun': 'Sun'*/}
-                {/*})}>correct all*/}
-                {/*</button>*/}
+                <button onClick={() => setAnswers({
+                    'Jupiter': 'Jupiter',
+                    'Orion': 'Orion',
+                    'Europa': 'Europa',
+                    'Lagrange': 'Lagrange',
+                    'Saturn': 'Saturn',
+                    'Betelgeuse': 'Betelgeuse',
+                    'Io': 'Io',
+                    'Titan': 'Titan',
+                    'Callisto': 'Callisto',
+                    'Hubble': 'Hubble',
+                    'Enceladus': 'Enceladus',
+                    'Sun': 'Sun'
+                })}>correct all
+                </button>
                 {areAnswersCorrect && <button onClick={() => setShowCards(!showCards)}>Toggle cards</button>}
 
-                <div className={`background ${removeBlur() ? 'background--removeBlur' : ''}`} style={{
-                    backgroundImage: `url(${answerImages[activeItem.answer]})`,
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    transition: 'all 800ms ease-in-out'
-                }}>
+                <div className={'background '}>
                     {
-                        areAnswersCorrect && <div className={'answer'}>
+                        areAnswersCorrect && <div className={'finalAnswer'}>
                             <motion.div transition={{duration: 2, delay: 1}} layoutId={'J'}>J</motion.div>
                             <motion.div transition={{duration: 2, delay: 1}} layoutId={'O'}>O</motion.div>
                             <motion.div transition={{duration: 2, delay: 1}} layoutId={'E'}>E</motion.div>
@@ -169,38 +211,48 @@ function App() {
                             <motion.div transition={{duration: 2, delay: 1}} layoutId={'S2'}>S</motion.div>
                         </div>
                     }
+                    <div className={'spline'}>
+                        {areAnswersCorrect && <Spline scene={'https://prod.spline.design/UnWOMLkS9rzBfEZy/scene.splinecode'}/>}
+                    </div>
                 </div>
                 {
                     data.quiz.map((item: any, index: number) => (
                         <motion.div
                             onMouseEnter={() => setActiveItem(item)}
-                            onMouseLeave={() => null}
+                            onMouseLeave={() => setActiveItem({} as QuizQuestion)}
                             initial={vCard.hidden}
                             animate={areAnswersCorrect && !showCards ? vCard.hidden : vCard.visible}
                             key={item.answer}
-                            className={'card'}>
-                            <h3>{item.question}</h3>
+                            className={`card ${removeBlur(item) ? 'card--noBlur' : '' }`}
+                            style={getStylePerImage(item)}
+                        >
+                            <h3 className={'question'}>{item.question}</h3>
 
                             {
                                 handleCorrect(item, index) ?
                                     <div
-                                        className={'card--answer'}>{item.answer.split('').map((l: string, li: number) =>
+                                        className={'answer'}>{item.answer.split('').map((l: string, li: number) =>
                                         li === 0 ? (areAnswersCorrect ? <h3>{l.toUpperCase()}</h3> :
                                                 <motion.h3 transition={{duration: 2, delay: 1}}
                                                            layoutId={(index === 10 || index === 11 || index === 12) ? l.toUpperCase() + '2' : l.toUpperCase()}>{l}</motion.h3>) :
                                             <h3>{l}</h3>)}</div> : <input
                                         className={handleCorrect(item, index) ? 'input--correct' : 'input--wrong'}
-                                        placeholder={'*'.repeat(item.answer.length)}
+                                        placeholder={'?'.repeat(item.answer.length)}
                                         disabled={handleCorrect(item, index)}
                                         value={answers[item.answer as keyof typeof answers]}
                                         onChange={(e) => handleOnChange(e.target.value, item.answer)}/>
                             }
 
-                            <ul>
-                                {item.hints.map((hint: string) => (
-                                    <li key={hint}>{hint}</li>
+                            <motion.ul variants={vUl} initial={'hidden'} animate={removeBlur(item) ? 'hidden' : 'visible'}>
+                                {item.hints.map((hint: string, hIndex) => (
+                                    <motion.li
+                                        variants={vLi}
+                                        initial={'visible'}
+                                        animate={removeBlur(item) ? 'hidden' : 'visible'}
+                                        transition={{delay: 0.2 * hIndex, duration: 0.8}}
+                                        key={hint}>{hint}</motion.li>
                                 ))}
-                            </ul>
+                            </motion.ul>
                         </motion.div>
                     ))
                 }
